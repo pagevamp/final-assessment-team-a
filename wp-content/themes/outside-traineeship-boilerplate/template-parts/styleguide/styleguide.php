7a437d4bd2725f3cbee035d6c3e76db057e73b9a
Krisha <krisha@Krishas-MacBook-Pro.local>
2024-12-06 16:54:54 +0545

    WTFA-11: Styleguide base

---
+++ wp-content/themes/outside-traineeship-boilerplate/template-parts/styleguide/styleguide.php
@@ -0,0 +1,398 @@
+<?php ?>
+<section class="styleguide">
+    <header>
+        <div class="heading">
+            <h2>StyleGuide - Development Gudielines</h2>
+        </div>
+    </header>
+
+    <main class="container">
+        <!-- aside bar -->
+        <aside id="sidebar" class="sidebar">
+            <div class="sidebar__list">
+                <ul class="list-group">
+                    <a href="#color">
+                        <li class="list-group-item">Colors</li>
+                    </a>
+                    <a href="#typography">
+                        <li class="list-group-item">Typography</li>
+                    </a>
+                    <a href="#icons">
+                        <li class="list-group-item">Icons</li>
+                    </a>
+                    <a href="#buttons">
+                        <li class="list-group-item">Buttons</li>
+                    </a>
+                    <a href="#texture">
+                        <li class="list-group-item">Texture</li>
+                    </a>
+                    <a href="#logo">
+                        <li class="list-group-item">Logo & Favicon</li>
+                    </a>
+                </ul>
+            </div>
+        </aside>
+
+        <div class="content">
+            <!-- color section -->
+            <section id="color" class="color">
+                <div class="color__container">
+                    <div class="color__head">
+                        <h2>Colors</h2>
+                    </div>
+                    <!-- Primary -->
+                    <div class="color__list">
+                        <div class="primary">
+                            <h3>Primary</h3>
+                            <div class="color__card orange">
+                                <div class="color__body">
+                                    <strong>Primary Red</strong>
+                                    <i>$primary</i>
+                                    <span>#D72027</span>
+                                </div>
+                                <code>
+                                    .bg-primary
+                                </code>
+                            </div>
+                            <div class="color__card charcoal">
+                                <div class="color__body">
+                                    <strong>Dark Red</strong>
+                                    <i>$dark-red</i>
+                                    <span>#A1181D</span>
+                                </div>
+                                <code>
+                                    .bg-dark-red
+                                </code>
+                            </div>
+                            <div class="color__card sky-blue">
+                                <div class="color__body">
+                                    <strong>Light Red</strong>
+                                    <i>$light-red</i>
+                                    <span>#F3BABC</span>
+                                </div>
+                                <code>
+                                .bg-light-red
+                            </code>
+                            </div>
+                            <div class="color__card stone">
+                            <div class="color__body">
+                                <strong>Ultra Light Red</strong>
+                                <i>$ultra-light-red</i>
+                                <span>#FBE9E9</span>
+                            </div>
+                            <code>
+                                .bg-ultra-light-red
+                            </code>
+                        </div>
+                    </div>
+                    <!-- Secondary -->
+                    <div class="color__list">
+                        <div class="secondary">
+                            <h3>Secondary</h3>
+                            <div class="color__card orange">
+                                <div class="color__body">
+                                    <strong>Secondary</strong>
+                                    <i>$secondary</i>
+                                    <span>#101922</span>
+                                </div>
+                                <code>
+                                    .bg-secondary
+                                </code>
+                            </div>
+                            <div class="color__card charcoal">
+                                <div class="color__body">
+                                    <strong>Dark</strong>
+                                    <i>$dark</i>
+                                    <span>#0C131A</span>
+                                </div>
+                                <code>
+                                .bg-dark
+                            </code>
+                            </div>
+                            <div class="color__card sky-blue">
+                                <div class="color__body">
+                                    <strong>Light</strong>
+                                    <i>$light</i>
+                                    <span>#DBDDDE</span>
+                                </div>
+                                <code>
+                                .bg-light
+                            </code>
+                            </div>
+                            <div class="color__card stone">
+                                <div class="color__body">
+                                    <strong>Ultra Light</strong>
+                                    <i>$ultra-light</i>
+                                    <span>#E7E8E9</span>
+                                </div>
+                                <code>
+                                    .bg-ultra-light
+                                </code>
+                        </div>
+                    </div>
+                    <!-- Neutral -->
+                    <div class="color__list">
+                        <div class="secondary">
+                            <h3>Neutrals</h3>
+                            <div class="color__card orange">
+                                <div class="color__body">
+                                    <strong>Neutral 600</strong>
+                                    <i>$neutral-600</i>
+                                    <span>#111111 </span>
+                                </div>
+                                <code>
+                                    .bg-neutral-600
+                                </code>
+                            </div>
+                            <div class="color__card charcoal">
+                                <div class="color__body">
+                                    <strong>Neutral 500</strong>
+                                    <i>$neutral-500</i>
+                                    <span>#1C1C1C</span>
+                                </div>
+                                <code>
+                                    .bg-neutral-500
+                                </code>
+                            </div>
+                            <div class="color__card sky-blue">
+                                <div class="color__body">
+                                    <strong>Neutral 400</strong>
+                                    <i>$neutral-400</i>
+                                    <span>#292929</span>
+                                </div>
+                                <code>
+                                    .bg-neutral-400
+                                </code>
+                            </div>
+                            <div class="color__card stone">
+                                <div class="color__body">
+                                    <strong>Neutral 300</strong>
+                                    <i>$neutral-300</i>
+                                    <span>#393939</span>
+                                </div>
+                                <code>
+                                    .bg-neutral-300
+                                </code>
+                            </div>
+                            <div class="color__card stone">
+                                <div class="color__body">
+                                    <strong>Neutral 200</strong>
+                                    <i>$neutral-200</i>
+                                    <span>#7C7C7C</span>
+                                </div>
+                                <code>
+                                    .bg-neutral-200
+                                </code>
+                            </div>
+                            <div class="color__card stone">
+                                <div class="color__body">
+                                    <strong>Neutral 100</strong>
+                                    <i>$neutral-100</i>
+                                    <span>#DEDEDE</span>
+                                </div>
+                                <code>
+                                    .bg-neutral-100
+                                </code>
+                            </div>
+                            <div class="color__card stone">
+                                <div class="color__body">
+                                    <strong>Neutral 75</strong>
+                                    <i>$neutral-75</i>
+                                    <span>#E9E9E9</span>
+                                </div>
+                                <code>
+                                    .bg-neutral-75
+                                </code>
+                            </div>
+                            <div class="color__card stone">
+                                <div class="color__body">
+                                    <strong>Neutral 50</strong>
+                                    <i>$neutral-50</i>
+                                    <span>#FFFFFF</span>
+                                </div>
+                                <code>
+                                    .bg-neutral-50
+                                </code>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Overlays -->
+                    <div class="color__list">
+                        <div class="secondary">
+                            <h3>Overlays</h3>
+                            <div class="color__card orange">
+                                <div class="color__body">
+                                    <strong>Overlay 68%</strong>
+                                    <i>$overlay-68</i>
+                                    <span>#000000AD</span>
+                                </div>
+                                <code>
+                                    .bg-overlay-68
+                                </code>
+                            </div>
+                            <div class="color__card charcoal">
+                                <div class="color__body">
+                                    <strong>Overlay 50%</strong>
+                                    <i>$overlay-50</i>
+                                    <span>#00000080</span>
+                                </div>
+                                <code>
+                                    .bg-overlay-50
+                                </code>
+                            </div>
+                            <div class="color__card sky-blue">
+                                <div class="color__body">
+                                    <strong>Overlay 30%</strong>
+                                    <i>$overlay-30</i>
+                                    <span>##0000004D</span>
+                                </div>
+                                <code>
+                                    .bg-overlay-30:
+                                </code>
+                            </div>
+                            <div class="color__card stone">
+                                <div class="color__body">
+                                    <strong>Overlay 20%</strong>
+                                    <i>$overlay-20</i>
+                                    <span>#00000033</span>
+                                </div>
+                                <code>
+                                    .bg-overlay-20
+                                </code>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- typography section -->
+            <section id="typography" class="typography">
+                <div class="typography__container">
+                    <div class="typography__header">
+                        <h2 class="typography__title">Typography</h2>
+                    </div>
+            
+                    <div class="typography__headings">
+                        <div class="typography__info">
+                            <h2 class="typography__info-title">h1, h2, h3, h4, h5, h6 - Headings</h2>
+                            <p class="typography__info-weight">Font-Weight: 500</p>
+                            <p class="typography__info-spacing">Letter Spacing: -3%</p>
+                        </div>
+            
+                        <div class="typography__heading typography__heading--h1">
+                            <h1 class="typography__heading-title">h1 - Heading 1</h1>
+                            <p class="typography__heading-size">Desktop-XL 64px/64px Mobile/tablet 40px/40px</p>
+                        </div>
+            
+                        <div class="typography__heading typography__heading--h2">
+                            <h2 class="typography__heading-title">h2 - Heading 2</h2>
+                            <p class="typography__heading-size">Desktop-XL 40px/44px Mobile/tablet 28px/30.8px</p>
+                        </div>
+            
+                        <div class="typography__heading typography__heading--h3">
+                            <h3 class="typography__heading-title">h3 - Heading 3</h3>
+                            <p class="typography__heading-size">Desktop-XL 32px/35.2px Mobile/tablet 20px/22px</p>
+                        </div>
+            
+                        <div class="typography__heading typography__heading--h4">
+                            <h4 class="typography__heading-title">h4 - Heading 4</h4>
+                            <p class="typography__heading-size">Desktop-XL 24px/26.4px Mobile/tablet 18px/19.8px</p>
+                        </div>
+            
+                        <div class="typography__heading typography__heading--h5">
+                            <h5 class="typography__heading-title">h5 - Heading 5</h5>
+                            <p class="typography__heading-size">Desktop-XL 16px/17.6px Mobile/tablet 14px/15.4px</p>
+                        </div>
+            
+                        <div class="typography__heading typography__heading--h6">
+                            <h6 class="typography__heading-title">h6 - Heading 6</h6>
+                            <p class="typography__heading-size">Desktop-XL 14px/15.4px Mobile/tablet 13px/14.3px</p>
+                        </div>
+                    </div>
+            
+                    <div class="typography__text">
+                        <div class="typography__text-header">
+                            <h2 class="typography__text-title">Text</h2>
+                        </div>
+                        <div class="typography__text-item typography__text-item--large">
+                            <p class="typography__text-label">Text Size Large</p>
+                            <p class="typography__text-size">Desktop - XL 24px/31.2px Mobile/tablet 20px/26px</p>
+                        </div>
+            
+                        <div class="typography__text-item typography__text-item--medium">
+                            <p class="typography__text-label">Text Size Medium</p>
+                            <p class="typography__text-size">Desktop - XL 20px/26px Mobile/tablet 16px/20.8px</p>
+                        </div>
+            
+                        <div class="typography__text-item typography__text-item--regular">
+                            <p class="typography__text-label">Text Size Regular</p>
+                            <p class="typography__text-size">Desktop - XL 16px/20.8px Mobile/tablet 14px/18.2px</p>
+                        </div>
+            
+                        <div class="typography__text-item typography__text-item--small">
+                            <p class="typography__text-label">Text Size Small</p>
+                            <p class="typography__text-size">Desktop - XL 14px/18.2px Mobile/tablet 13px/16.9px</p>
+                        </div>
+            
+                        <div class="typography__text-item typography__text-item--xsmall">
+                            <p class="typography__text-label">Text Size XSmall</p>
+                            <p class="typography__text-size">Desktop/Mobile/Tablet - XL 12px/15.6px</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- icon section -->
+            <section id="icons" class="icons">
+                <div class="icons__container">
+                    <div class="icons__header">
+                        <h2 class="icons__title">Icons</h2>
+                    </div>
+                    <div class="icons__lists">
+                        <div class="icons__list--i1">
+                            <span class="icon icon-xmark"></span>
+                            <code>&lt;span class="icon-xmark"&gt;&lt;/span&gt;</code> 
+                            <!-- &lt means less than and &gt means greater than -->
+                        </div>
+                        <div class="icons__list--i1">
+                            <span class="icon icon-user1"></span>
+                            <code>&lt;span class="icon-user1"&gt;&lt;/span&gt;</code> 
+                        </div>
+                        <div class="icons__list--i1">
+                            <span class="icon icon-icon-right"></span>
+                            <code>&lt;span class="icon-icon-right"&gt;&lt;/span&gt;</code> 
+                        </div>
+                        <div class="icons__list--i1">
+                            <span class="icon icon-search1"></span>
+                            <code>&lt;span class="icon-search1"&gt;&lt;/span&gt;</code> 
+                        </div>
+                        <div class="icons__list--i1">
+                            <span class="icon icon-menu-burger"></span>
+                            <code>&lt;span class="icon-menu-burger"&gt;&lt;/span&gt;</code> 
+                        </div>
+                        <div class="icons__list--i1">
+                            <span class="icon icon-icon-sharp"></span>
+                            <code>&lt;span class="icon-icon-sharp"&gt;&lt;/span&gt;</code> 
+                        </div>
+                        <div class="icons__list--i1">
+                            <span class="icon icon-icon-marker"></span>
+                            <code>&lt;span class="icon-icon-marker"&gt;&lt;/span&gt;</code> 
+                        </div>
+                        <div class="icons__list--i1">
+                            <span class="icon icon-icon-empty"></span>
+                            <code>&lt;span class="icon-icon-empty"&gt;&lt;/span&gt;</code> 
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+
+    <footer>
+        <div class="foot">
+            <p class="mb-0">© 2024. All Rights Reserved.</p>
+        </div>
+    </footer>
+
+</section>
+
---
+++ wp-content/themes/outside-traineeship-boilerplate/templates/template_styleguide.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+Template Name: Styleguide Template
+*/
+
+get_header();
+
+get_template_part('template-parts/styleguide/styleguide');
+// the_content();
+
+get_footer();
