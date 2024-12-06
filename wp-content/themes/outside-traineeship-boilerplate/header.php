9added3044e8fef88fc89f71f344fd3f13f73f6a
Krisha <krisha@Krishas-MacBook-Pro.local>
2024-12-06 12:01:41 +0545

    WTFA-18: Header Backend done
    WTFA-17 : header frontend first draft

--- wp-content/themes/outside-traineeship-boilerplate/header.php
+++ wp-content/themes/outside-traineeship-boilerplate/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,5 +23,36 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<?php
+		$header		= get_field('header_content', 'option');
+		$logo		= $header['logo'];
+		$emergency	= $header['emergency_contact'];
+		?>
+
+		<header id="header" class="header">
+			<div class="header__logo-container">
+				<?php if (!empty($logo)): ?>
+					<img class="header__logo" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>">
+				<?php endif; ?>
+			</div>
+
+			<?php
+			wp_nav_menu(array(
+				'theme_location'	=> 'header-menu',
+				'menu_class'     	=> 'header__menu',
+				'menu_id'			=> 'header-menu',
+				'container'			=> 'nav',
+				'container_class'	=> 'header__nav'
+			));
+			?>
+
+			<div class="header__contact">
+				<?php if (!empty($emergency)): ?>
+					<p><?php echo $emergency['heading']; ?></p>
+					<p><span class="icon-phone-call"></span> <?php echo $emergency['phone']; ?></p>
+				<?php endif; ?>
+			</div>
+
+		</header>
