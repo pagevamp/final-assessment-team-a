5d76daa7d7bed1f4a64f63e204bd3460c4b3fa8e
SharadShakya9 <shakyasharad9@gmail.com>
2024-12-06 14:38:54 +0545

    WTFA-22 : acf updated

--- wp-content/themes/outside-traineeship-boilerplate/acf-block.php
+++ wp-content/themes/outside-traineeship-boilerplate/acf-block.php
@@ -3,7 +3,7 @@ add_action('acf/init', function() {
     if( function_exists('acf_register_block_type') ) {
         $biolerplateModules = [
             'leadspace'  => 'Leadspace',
-            'departments' => 'Departments',
+            'department' => 'Department',
         ];
 
         foreach($biolerplateModules as $key => $mModule) {
