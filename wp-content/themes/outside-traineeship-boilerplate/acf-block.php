54748b4059e1ac06fcaa62bcd1019216c4b069a9
SharadShakya9 <shakyasharad9@gmail.com>
2024-12-06 13:55:13 +0545

    WTFA-23 : Services backend acf created

--- wp-content/themes/outside-traineeship-boilerplate/acf-block.php
+++ wp-content/themes/outside-traineeship-boilerplate/acf-block.php
@@ -3,6 +3,7 @@ add_action('acf/init', function() {
     if( function_exists('acf_register_block_type') ) {
         $biolerplateModules = [
             'leadspace'  => 'Leadspace',
+            'departments' => 'Departments',
         ];
 
         foreach($biolerplateModules as $key => $mModule) {
