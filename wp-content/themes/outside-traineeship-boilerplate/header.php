9bd98a0f5e28dc76662423d5313f5129d2ee59fd
SharadShakya9 <shakyasharad9@gmail.com>
2024-12-07 20:47:10 +0545

    WTFA-17 : Header developed

--- wp-content/themes/outside-traineeship-boilerplate/header.php
+++ wp-content/themes/outside-traineeship-boilerplate/header.php
@@ -31,28 +31,72 @@
 		$emergency	= $header['emergency_contact'];
 		?>
 
-		<header id="header" class="header">
-			<div class="header__logo-container">
-				<?php if (!empty($logo)): ?>
-					<img class="header__logo" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>">
-				<?php endif; ?>
-			</div>
+		<header id="header" class="header" role="banner">
+			<nav class="navbar navbar-expand-lg navbar-light" role="navigation">
+				<div class="container-fluid">
+					<!-- Logo Section -->
+					<div class="header__logo-container navbar-brand">
+						<?php if (!empty($logo)): ?>
+							<a href="#" aria-label="Homepage">
+								<img class="header__logo" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>">
+							</a>
+						<?php endif; ?>
+					</div>
 
-			<?php
-			wp_nav_menu(array(
-				'theme_location'	=> 'header-menu',
-				'menu_class'     	=> 'header__menu',
-				'menu_id'			=> 'header-menu',
-				'container'			=> 'nav',
-				'container_class'	=> 'header__nav'
-			));
-			?>
+					<!-- Toggle Button for Offcanvas Menu -->
+					<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#header-menu" aria-controls="#header-menu" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'your-theme-slug'); ?>">
+						<span class="icon-menu"></span>
+					</button>
 
-			<div class="header__contact">
-				<?php if (!empty($emergency)): ?>
-					<p><?php echo $emergency['heading']; ?></p>
-					<p><span class="icon-phone-call"></span> <?php echo $emergency['phone']; ?></p>
-				<?php endif; ?>
-			</div>
+					<!-- Offcanvas Menu -->
+					<div class="offcanvas offcanvas-end" tabindex="-1" id="header-menu" aria-labelledby="offcanvasLabel">
+						<div class="offcanvas-header">
+							<div class="header__logo-container offcanvas-title" id="offcanvasLabel">
+								<?php if (!empty($logo)): ?>
+									<a href=" #">
+										<img class="header__logo" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>">
+									</a>
+								<?php endif; ?>
+							</div>
+							<button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
+								<span class="icon-x-mark"></span>
+							</button>
+						</div>
+
+						<!-- Primary Navigation menu -->
+						<?php
+						wp_nav_menu(array(
+							'theme_location'	=> 'header-menu',
+							'depth'				=> 2,
+							'container'			=> 'false',
+							'menu_class'      	=> 'navbar-nav text-lg me-auto',
+							'fallback_cb'     	=> 'WP_Bootstrap_Navwalker::fallback',
+							'walker'          	=> new WP_Bootstrap_Navwalker(),
+						));
+						?>
+
+						<!-- Emergency Contact Information in Offcanvas -->
+						<div class="offcanvas-header">
+							<span class="icon-linkedin" aria-hidden="true"></span>
+							<div class="header__emergency">
+								<?php if (!empty($emergency)): ?>
+									<p class="text-xsm text-neutral-600"><?php echo $emergency['heading']; ?></p>
+									<p class="text-lg"><span class="icon-phone-call" aria-hidden="true"></span> <?php echo $emergency['phone']; ?></p>
+								<?php endif; ?>
+							</div>
+						</div>
+					</div>
+
+					<!-- Main Header Contact Info -->
+					<div class="header__contact" aria-label="Emergency contact">
+						<a target="_blank" href="tel:<?php echo $emergency['phone']; ?>" class="header__emergency">
+							<?php if (!empty($emergency)): ?>
+								<p class="text-xsm text-neutral-600"><?php echo $emergency['heading']; ?></p>
+								<p class="text-lg text-neutral-600"><span class="icon-phone-call"></span> <?php echo $emergency['phone']; ?></p>
+							<?php endif; ?>
+						</a>
+					</div>
+				</div>
+			</nav>
 
 		</header>
--- wp-content/themes/outside-traineeship-boilerplate/webpack.mix.js
+++ wp-content/themes/outside-traineeship-boilerplate/webpack.mix.js
@@ -1,51 +1,50 @@
-const fs = require('fs');
-const path = require('path');
-const mix = require('laravel-mix');
+const fs = require("fs")
+const mix = require("laravel-mix")
+const path = require("path")
 
-const styleFiles = fs.readdirSync(path.resolve(__dirname, 'resources', 'styles', 'styles'), 'utf-8');
-const scriptFiles = fs.readdirSync(path.resolve(__dirname, 'resources', 'scripts', 'modules'), 'utf-8');
+const styleFiles = fs.readdirSync(
+  path.resolve(__dirname, "resources", "styles", "styles"),
+  "utf-8"
+)
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Sage application. By default, we are compiling the Sass file
- | for your application, as well as bundling up your JS files.
- |
- */
+const scriptFiles = fs.readdirSync(
+  path.resolve(__dirname, "resources", "scripts", "modules"),
+  "utf-8"
+)
 
-mix
-  .setPublicPath('./public')
-  .browserSync('wordpress-boilerplate-trainee.local');
+// /*
+//  |--------------------------------------------------------------------------
+//  | Mix Asset Management
+//  |--------------------------------------------------------------------------
+//  |
+//  | Mix provides a clean, fluent API for defining some Webpack build steps
+//  | for your Sage application. By default, we are compiling the Sass file
+//  | for your application, as well as bundling up your JS files.
+//  |
+//  */
+
+mix.setPublicPath("./public").browserSync("wordpress-boilerplate-trainee.local")
 
 for (let style of styleFiles) {
-  mix.sass(`resources/styles/styles/${style}`, 'styles/modules');
+  mix.sass(`resources/styles/styles/${style}`, "styles/modules")
 }
 
 mix
-  .sass('resources/styles/app.scss', 'styles')
-  .sass('resources/styles/editor.scss', 'styles')
+  .sass("resources/styles/app.scss", "styles")
+  .sass("resources/styles/editor.scss", "styles")
+
   .webpackConfig({
-    devtool: 'source-map'
-  });
+    devtool: "source-map",
+  })
 
 for (let script of scriptFiles) {
-  mix.js(`resources/scripts/modules/${script}`, 'scripts')
-    .autoload({ jquery: ['$', 'window.jQuery'] })
-    .extract();
+  mix.js(`resources/scripts/modules/${script}`, "scripts")
 }
 
-mix
-  .js('resources/scripts/app.js', 'scripts')
-  .autoload({ jquery: ['$', 'window.jQuery'] })
-  .extract();
+mix.js("resources/scripts/app.js", "scripts")
 
 mix
-  .copyDirectory('resources/images', 'public/images')
-  .copyDirectory('resources/fonts', 'public/fonts');
+  .copyDirectory("resources/images", "public/images")
+  .copyDirectory("resources/fonts", "public/fonts")
 
-mix
-  .sourceMaps()
-  .version();
+// mix.sourceMaps().version()
