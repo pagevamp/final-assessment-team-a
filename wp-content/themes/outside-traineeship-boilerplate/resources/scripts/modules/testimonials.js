b73bdfbf03b5781e1a0b4892d54780427a0db65a
Krisha <krisha@Krishas-MacBook-Pro.local>
2024-12-12 17:46:49 +0545

    WTFA-27: Testimonials updated

--- wp-content/themes/outside-traineeship-boilerplate/resources/scripts/modules/testimonials.js
+++ wp-content/themes/outside-traineeship-boilerplate/resources/scripts/modules/testimonials.js
@@ -1,20 +1,28 @@
 const swiper = new Swiper(".swiper", {
-  // loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    navigation: {
-      nextEl: ".btn-primary-next",
-      prevEl: ".btn-primary-prev",
-    },
-    on: {
-      slideChange: function() {
-        // Get the current index (starts from 0)
-        let currentIndex = this.realIndex + 1; 
-
-        // Update the progress numbers
-        document.getElementById('left-number').innerText = String(currentIndex).padStart(2, '0');         
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".btn-primary-next",
+    prevEl: ".btn-primary-prev",
+  },
+  autoplay: {
+    enabled: false,
+  },
+  breakpoints: {
+    768: {
+      autoplay: {
+        enabled: true,
+        delay: 1000,
       }
     }
+  },
+  on: {
+    slideChange: function () {
+      // Update progress numbers
+      let currentIndex = this.realIndex + 1;
+      document.getElementById("left-number").innerText = String(currentIndex).padStart(2, "0");
+    },
+  },
 });
