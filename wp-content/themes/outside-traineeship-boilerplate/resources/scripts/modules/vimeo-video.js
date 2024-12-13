cd00549e2c6373922d339e4133d8d2350d363a9c
SharadShakya9 <shakyasharad9@gmail.com>
2024-12-13 10:07:04 +0545

    WTFA-49 : Vimeo player api issue

--- wp-content/themes/outside-traineeship-boilerplate/resources/scripts/modules/vimeo-video.js
+++ wp-content/themes/outside-traineeship-boilerplate/resources/scripts/modules/vimeo-video.js
@@ -1,4 +1,18 @@
+import Player from '@vimeo/player';
+
 document.addEventListener('DOMContentLoaded', function() {
     const iframe = document.getElementById('vimeo-player');
     const player = new Vimeo.Player(iframe);
+    const playButton = document.getElementById('play-button');
+    const pauseButton = document.getElementById('pause-button');
+
+    // Play button functionality
+    playButton.addEventListener('click', () => {
+        player.play();
+    })
+
+    // Pause button functionality
+    pauseButton.addEventListener('click', () => {
+        player.pause();
+    })
 })
