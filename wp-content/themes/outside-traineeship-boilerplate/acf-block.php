371c558766a8af89f44d9f3ec033629533c9f608
SharadShakya9 <shakyasharad9@gmail.com>
2024-12-16 11:04:58 +0545

    PR2-WTFA-56 : acf block created

--- wp-content/themes/outside-traineeship-boilerplate/acf-block.php
+++ wp-content/themes/outside-traineeship-boilerplate/acf-block.php
@@ -12,6 +12,7 @@ add_action('acf/init', function() {
             'leadership' => 'Leadership',
             'vimeo_video' => 'Vimeo Video',
             'history'   => 'History',
+            'recent_projects'   => "Recent Projects",
         ];
 
         foreach($biolerplateModules as $key => $mModule) {
