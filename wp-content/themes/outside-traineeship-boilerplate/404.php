324bb5419a9d4a583bd0d69bca58e5ea6f5aa5d4
Krisha <krisha@Krishas-MacBook-Pro.local>
2024-12-18 01:25:16 +0545

    temp: Updating 404

--- wp-content/themes/outside-traineeship-boilerplate/404.php
+++ wp-content/themes/outside-traineeship-boilerplate/404.php
@@ -11,44 +11,47 @@ get_header();
 
 	$error_404 = get_field('404_error', 'option');
 
-	$error_code = $error_404['error_code'];
-	$error_message = $error_404['error_message'];
-	$error_description = $error_404['error_description'];
-	$button = $error_404['button'];
-	$button_label = $button['button_label'];
-	$page_link = $button['page_link'];
-	$image = $error_404['background_image'];
+	if(!empty($error_404)): 
+		$error_code = $error_404['error_code'];
+		$error_message = $error_404['error_message'];
+		$error_description = $error_404['error_description'];
+		$button = $error_404['button'];
+		$button_label = $button['button_label'];
+		$page_link = $button['page_link'];
+		$image = $error_404['background_image'];
 ?>
 
-	<main id="primary" class="site-main">
+		<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<div class="container p-0">
-				<?php if ($image): ?>
-					<img src="<?php echo $image['url'];?>" class="error__image" alt="<?php echo $image['alt'];?>" aria-label="Error background image">
-				<?php endif; ?>
-
-				<div class="error__content">
-					<?php if ($error_code): ?>
-						<p class="c1"><?php echo $error_code; ?></p>
+			<section class="error-404 not-found">
+				<div class="container p-0">
+					<?php if ($image): ?>
+						<img src="<?php echo $image['url'];?>" class="error__image" alt="<?php echo $image['alt'];?>" aria-label="Error background image">
 					<?php endif; ?>
 
-					<?php if ($error_message): ?>
-						<h2 class="d2 pb-m"><?php echo $error_message; ?></h2>
-					<?php endif; ?>
+					<div class="error__content">
+						<?php if ($error_code): ?>
+							<p class="c1"><?php echo $error_code; ?></p>
+						<?php endif; ?>
 
-					<?php if ($error_description): ?>
-						<p class="sh3 pb-s"><?php echo $error_description; ?></p>
-					<?php endif; ?>
+						<?php if ($error_message): ?>
+							<h2 class="d2 pb-m"><?php echo $error_message; ?></h2>
+						<?php endif; ?>
 
-					<?php if ($button): ?>
-						<a href="<?php echo $page_link;?>" class="btn-primary text-decoration-none mx-auto"aria-label="Navigate to <?php echo $button_label; ?> page"><?php echo $button_label; ?></a>
-					<?php endif; ?>
+						<?php if ($error_description): ?>
+							<p class="sh3 pb-s"><?php echo $error_description; ?></p>
+						<?php endif; ?>
+
+						<?php if ($button): ?>
+							<a href="<?php echo $page_link;?>" class="btn-primary text-decoration-none mx-auto"aria-label="Navigate to <?php echo $button_label; ?> page"><?php echo $button_label; ?></a>
+						<?php endif; ?>
+					</div>
 				</div>
-			</div>
-		</section><!-- .error-404 -->
+			</section><!-- .error-404 -->
 
-	</main><!-- #main -->
+		</main><!-- #main -->
 
-<?php
+	<?php
+	endif;
+	
 get_footer();
