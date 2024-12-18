80b1c436bf48fec440787f84df7821e2502a271d
SharadShakya9 <shakyasharad9@gmail.com>
2024-12-18 12:21:02 +0545

    PR2-WTFA-48 : if checks added

--- wp-content/themes/outside-traineeship-boilerplate/blocks/vimeo-video.php
+++ wp-content/themes/outside-traineeship-boilerplate/blocks/vimeo-video.php
@@ -1,16 +1,17 @@
 <?php
 $video = get_field('video');
+if (!empty($video)):
 ?>
+    <section id="gallery" class="gallery">
+        <div class="gallery__video-container">
+            <iframe id="vimeo-player" class="gallery__video" src="<?php echo $video['vimeo_link']['url']; ?>&controls=0" data-src="<?php echo $video['vimeo_link']['url']; ?>&controls=0" frameborder="0" allow="autoplay"></iframe>
+            <div class="overlay bg-overlay-50"></div>
 
-<section id="gallery" class="gallery">
-    <div class="gallery__video-container">
-        <iframe id="vimeo-player" class="gallery__video" src="<?php echo $video['vimeo_link']['url']; ?>&controls=0" data-src="<?php echo $video['vimeo_link']['url']; ?>&controls=0" frameborder="0" allow="autoplay" ></iframe>
-        <div class="overlay bg-overlay-50" ></div>
-
-        <div class="gallery__controls">            
-            <div class="btn-play-pause">
-                <span class="icon-play"></span>
+            <div class="gallery__controls">
+                <div class="btn-play-pause">
+                    <span class="icon-play"></span>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
